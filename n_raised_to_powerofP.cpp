227a5fd4f3e95eef908064bc4efac12db499a3da
Nazmul Hasan <Nazmul.Hasan@g.bracu.ac.bd>
2024-09-16 23:48:40 +0600

    .\n_raised_to_powerofP.cpp

---
+++ n_raised_to_powerofP.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int power(int n, int p)
+{
+    if (p == 0)
+        return 1;
+
+    int next = power(n, p - 1);
+
+    return n * next;
+}
+
+int main()
+{
+    int n, p;
+    cin >> n >> p;
+
+    cout << power(n, p);
+
+    return 0;
+}
