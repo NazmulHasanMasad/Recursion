eb15712c76a15d11842bca8807043d24146055da
Nazmul Hasan <Nazmul.Hasan@g.bracu.ac.bd>
2025-09-07 17:26:45 +0600

    .\2_count_paths.cpp

---
+++ 2_count_paths.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int countPaths(int s, int e)
+{
+    if (s == e)
+    {
+        return 1;
+    }
+    if (s > e)
+    {
+        return 0;
+    }
+
+    int count = 0;
+
+    for (int i = 1; i <= 6; i++)
+    {
+        count += countPaths(s + i, e);
+    }
+    return count;
+}
+
+int main()
+{
+    cout << countPaths(1, 10);
+
+    return 0;
+}
